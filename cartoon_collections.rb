1b715f85b54f4986b651add841702c1dfc01bbb7
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 02:51:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,12 @@ def long_planeteer_calls(calls_long)
   calls_long.any?{|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  maybe_cheese = %w(cheddar gouda camembert)
+  array.find do |cheese|
+maybe_cheese.include?(cheese) 
+ end
 end
+# # code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
