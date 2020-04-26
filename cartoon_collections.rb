b6e42bf5f948071932c601390565cae66d1cb807
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-26 02:41:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  array = []
+  names.each_with_index do |name, index| 
+    puts array << "#{index + 1}. #{name}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+def summon_captain_planet(planeteer)
+  planeteer_calls = planeteer.map do |captain|
+    captain.capitalize << "!"
+   end
+  end
+  
+#   def summon_captain_planet(array)
+#   array.collect do |word|
+#     #word.capitalize + "!"
+#     word.capitalize << "!"
+#   end
+# end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  
+  # code an argument here
   # Your code here
 end
 
